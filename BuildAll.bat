9bbb2d5bf90ab0f6e83953f66db9d05b822b7ef6
Sameer M. Ali <mycoder@live.com>
2013-12-16 20:12:05 +0200

    Update BuildAll.bat
    Publish folder name moved to parameter.

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,27 +1,27 @@
 set version=1.0.5.1
 set mode=%1
+set pubfolder=pub
 
 echo Building AllSearch52 Started******************
 echo .
-call .\build AllSearch52 https://secureinstall.us/%mode%/AllSearch52/ %version%
+call .\build AllSearch52 https://secureinstall.us/%mode%/AllSearch52/ %version% %pubfolder%
 echo Building AllSearch52 Ended******************
 echo .
 
 echo Building Fastrank Started******************
 echo .
-call .\build Fastrank https://secureinstall.us/%mode%/Fastrank/ %version%
+call .\build Fastrank https://secureinstall.us/%mode%/Fastrank/ %version% %pubfolder%
 echo Building Fastrank Ended******************
 echo .
 
 echo Building ListingApe Started******************
 echo .
-call .\build ListingApe https://secureinstall.us/%mode%/ListingApe/ %version%
+call .\build ListingApe https://secureinstall.us/%mode%/ListingApe/ %version% %pubfolder%
 echo Building ListingApe Ended******************
 echo .
 
 echo Building TownCenter Started******************
 echo .
-call .\build TownCenter https://secureinstall.us/%mode%/TownCenter/ %version%
+call .\build TownCenter https://secureinstall.us/%mode%/TownCenter/ %version% %pubfolder%
 echo Building TownCenter Ended******************
 echo .
-

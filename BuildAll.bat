70fe00eb346daa8602e4117866afe1138daba469
Sameer M. Ali <mycoder@live.com>
2013-12-16 21:03:00 +0200

    Update BuildAll.bat

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,27 +1,38 @@
+ECHO OFF
+CLS
+IF NOT "%1"=="" GOTO :Continue
+echo .
+echo .
+echo .
+echo usage:    BuildAll [Mode]
+goto :eof
+
+:Continue
+
 set version=1.0.5.1
 set mode=%1
-set pubfolder=pub
+set pubfolder=.\deploy\
 
-echo Building AllSearch52 Started******************
-echo .
-call .\build AllSearch52 https://secureinstall.us/%mode%/AllSearch52/ %version% %pubfolder%
-echo Building AllSearch52 Ended******************
-echo .
+echo ******************Building AllSearch52 Started******************
+echo .******************^^^^^^^^^^^^^^^^^^^^^^^^^^******************
+call .\build AllSearch52 https://secureinstall.us/%mode%/AllSearch52/ %version% %pubfolder% %mode%
+echo ******************Building AllSearch52 Ended******************
+echo .******************^^^^^^^^^^^^^^^^^^^^^^^^******************
 
-echo Building Fastrank Started******************
+echo ******************Building Fastrank Started******************
 echo .
-call .\build Fastrank https://secureinstall.us/%mode%/Fastrank/ %version% %pubfolder%
-echo Building Fastrank Ended******************
-echo .
+call .\build Fastrank https://secureinstall.us/%mode%/Fastrank/ %version% %pubfolder% %mode%
+echo ******************Building Fastrank Ended******************
+echo .******************^^^^^^^^^^^^^^^^^^^^^******************
 
-echo Building ListingApe Started******************
+echo ******************Building ListingApe Started******************
 echo .
-call .\build ListingApe https://secureinstall.us/%mode%/ListingApe/ %version% %pubfolder%
-echo Building ListingApe Ended******************
-echo .
+call .\build ListingApe https://secureinstall.us/%mode%/ListingApe/ %version% %pubfolder% %mode%
+echo ******************Building ListingApe Ended******************
+echo .******************^^^^^^^^^^^^^^^^^^^^^^^******************
 
-echo Building TownCenter Started******************
+echo ******************Building TownCenter Started******************
 echo .
-call .\build TownCenter https://secureinstall.us/%mode%/TownCenter/ %version% %pubfolder%
-echo Building TownCenter Ended******************
-echo .
+call .\build TownCenter https://secureinstall.us/%mode%/TownCenter/ %version% %pubfolder% %mode%
+echo ******************Building TownCenter Ended******************
+echo .******************^^^^^^^^^^^^^^^^^^^^^^^******************
